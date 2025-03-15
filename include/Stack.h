b8ac66190133b810e3adbeb950511c393873d90e
Giova <giovabuchieri@gmail.com>
2025-03-15 18:37:48 -0300

    Stack finish

--- include/Stack.h
+++ include/Stack.h
@@ -7,14 +7,20 @@
 #ifndef STACK_H
 #define STACK_H
 
+typedef struct StackNode {
+    Node *node;
+    struct StackNode *next;
+} StackNode;
+
 typedef struct Stack {
-    Node *top;
-    struct Stack *next;
+    StackNode *top;
 } Stack;
 
 // Funciones de la pila
-Node* first(Stack *stack);
-Node* pop(Stack **stack);
-void push(Stack **stack, Node *node);
+Stack* createStack();
+void push(Stack *stack, Node *node);
+void pop(Stack *stack);
+Node* top(Stack *stack);
+int isEmpty(Stack *stack);
 
 #endif // STACK_H
--- src/Stack.c
+++ src/Stack.c
@@ -1,6 +1,45 @@
 
 #include "../include/Stack.h"
 
-Node* first(Stack *stack) {
-    stack->next;
+Stack* createStack() { 
+    Stack *inicTop = (Stack *)malloc(sizeof(Stack));
+    inicTop->top = NULL;
+    return inicTop;
 }
+
+void push(Stack *stack, Node *node) {
+    StackNode *newTop = (StackNode *)malloc(sizeof(StackNode));
+
+    if (!newNode) {
+        printf("Error \n");
+        return;
+    }
+
+    StackNode *oldTop = stack->top;
+
+    newTop->node = node;
+    newTop->next = oldTop;
+    stack->top = newTop;
+
+}
+
+void pop(Stack *stack) {
+    if (stack->top == NULL) {
+        printf("Pila vacía\n");
+        return;
+    }
+
+    StackNode *oldTop = stack->top;
+    stack->top = oldTop->next;
+}
+
+Node* top(Stack *stack) {
+    if (stack->top == NULL) return NULL;
+    return stack->top->node;
+}
+
+int isEmpty(Stack *stack) {
+    return stack->top == NULL;
+}
+
+
