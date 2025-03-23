d9b5e630339da5e03653aa421ad18086027915d8
Giova <giovabuchieri@gmail.com>
2025-03-23 19:18:19 -0300

    algorithm_3 completado

--- src/Algorithms.c
+++ src/Algorithms.c
@@ -30,8 +30,9 @@ void collapseSCC(Graph *G) {
     Graph *currentGraph = G;
     while (currentGraph != NULL) {
         if (getDValue(D, currentGraph->node) == UNVISITED) {
-            //ALGORITMO 3 QUE NO IMPLEMENTE TODAVIA
+            visitNode(currentGraph->node, &I);
         }
+        currentGraph = currentGraph->next;
     }
 
     currentGraph = G;
@@ -40,6 +41,7 @@ void collapseSCC(Graph *G) {
         if (getRValue(R, currentGraph->node) != currentGraph->node) {       //Ver si los nodos puedo compararlos asi (creeria que si ya que contienen la misma direccion de memoria porque no se crean nuevos nodos)
             //unify(currentGraph->node, getRValue(currentGraph->node))                                                       //TODAVIA NO LO IMPLEMENTE
         }
+        currentGraph = currentGraph->next;
     }
 }
 
@@ -54,16 +56,22 @@ void visitNode(Node* v, int *I) {
     Node* edges[MAX_NODES] = v->edges;
 
     for (int i = 0; i < MAX_NODES && edges[i] != NULL; i++) {
-        Node *currentNode = edges[i];
-        if (getDValue(D, currentNode) == UNVISITED) visitNode(currentNode, &I);
+        Node *w = edges[i];
+        if (getDValue(D, w) == UNVISITED) visitNode(w, I);
 
-        if (!existsInSet(C,currentNode)) {
+        if (!existsInSet(C,w)) {
             //R(v) ← (D(R(v)) < D(R(w))) ? R(v) : R(w)
+            Node *RvalueInV = getRValue(R,v);
+            Node *RvalueInW = getRValue(R,w);
+            if (getDValue(D, RvalueInV) > getDValue(D, RvalueInW)) {
+                setRValue(R, v, RvalueInW);
+            }
+            
         }
     }
     
     if (getRValue(R,v) == v) {
-        addToCSet(&C,v);
+        addToCSet(C,v);
 
         while (!isEmpty(S)) {
             //let w be the node on the top of S
@@ -81,3 +89,6 @@ void visitNode(Node* v, int *I) {
         push(S,v);
     }
 }
+
+
+//Algorithm 4
