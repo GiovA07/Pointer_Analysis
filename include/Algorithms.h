b955f9ce14fd7a7d9f578d5158d56db4eec35df4
Giova <giovabuchieri@gmail.com>
2025-09-09 17:24:35 -0300

    cambios en ListContraint

--- include/Algorithms.h
+++ include/Algorithms.h
@@ -1,7 +1,6 @@
 #ifndef ALGORITHMS_H
 #define ALGORITHMS_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -17,16 +16,16 @@
 #define Pcur(n) ((n)->references)
 #define Pold(n) ((n)->pold)
 
-extern ListConstraint *listComplex1; // l ⊇ *r
-extern ListConstraint *listComplex2; // *l ⊇ r
+extern ListConstraint *listComplex1;                    // l ⊇ *r
+extern ListConstraint *listComplex2;                    // *l ⊇ r
 
-void wave_Propagation(Graph **G);       //Algorithm 1
-void collapseSCC(Graph **G);            //Algorithm 2
-void mergeNodes(Node *v, Node *w);      
-void visitNode(Node* v, int *I);        //Algorithm 3
-void unify(Graph **G, Node *target, Node *source); //Unifica los nodos para que se vuelvan 1 solo.
-void perform_Wave_Propagation();        //Algorithm 4
-void propagationTo(Node *w, Set *pdif); //Propaga el conjunto de apuntados a un nodo en particular.
-bool add_new_edges();                    //Algorithm 5
+void wave_Propagation(Graph **G);                       //Algorithm 1
+void collapseSCC(Graph **G);                            //Algorithm 2
+void mergeNodes(Node *v, Node *w);                      
+void visitNode(Node* v, int *I);                        //Algorithm 3
+void unify(Graph **G, Node *target, Node *source);      //Unifica los nodos para que se vuelvan 1 solo.
+void perform_Wave_Propagation();                        //Algorithm 4
+void propagationTo(Node *w, Set *pdif);                 //Propaga el conjunto de apuntados a un nodo en particular.
+bool add_new_edges();                                   //Algorithm 5
 void remap_constraints_after_unify(Node *oldw, Node *rep);
 #endif
--- src/ListConstraint.c
+++ src/ListConstraint.c
@@ -16,8 +16,6 @@ void addConstraint(ListConstraint **list, Node *a, Node *b) {
     newConstraint->next = *list;  
     newConstraint->pcache = createSet();
     *list = newConstraint;
-
-
 }
 
 Node* constraint_getL (ListConstraint *list) {
@@ -40,30 +38,29 @@ void constraint_setR(ListConstraint *list, Node *rep) {
     list->dereferenced = rep;
 }
 void constraint_setCache(ListConstraint *list, Set *newCache) {
-    Set *exCache = list->pcache;
+    if (!list) return;
+    Set *old = list->pcache;
     list->pcache = newCache;
-    free(exCache);
+    if (old && old != newCache) 
+        set_destroy(old);
 }
+
 ListConstraint* constraint_getNext(ListConstraint *list) {
     return list->next;
 }
 
-
 // a ⊇ ∗b
 void printConstraintComplex1(ListConstraint *list) {
     printf("Complex 1 Constraints:\n");
-    while (list) {
+    for (; list; list = list->next)
         printf("%s ⊇ *%s\n", list->superset->name, list->dereferenced->name);
-        list = list->next;
-    }
 }
 
 // ∗a ⊇ b
 void printConstraintComplex2(ListConstraint *list) {
     printf("Complex 2 Constraints:\n");
-    while (list) {
+    for (; list; list = list->next)
         printf("*%s ⊇ %s\n", list->superset->name, list->dereferenced->name);
-        list = list->next;
-    }
 }
+
 
